5ad04353f5b4e0fde1e01a045e90d23d4d8e7095
Muhammad Saqib <msaqib@gordiansol.com>
2021-02-09 13:04:44 +0500

    react reset/forget-password

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Registration from "./components/auth/registration";
 import LoginForm from "./components/auth/login";
 import Logout from "./components/auth/logout";
 import ProtectedRoute from "./components/common/protectedRoute";
+import ForgetPassword from "./components/auth/forgetPassword";
+import ResetPassword from "./components/auth/resetPassword";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
           <Route path="/logout" component={Logout} />
           {/* <ProtectedRoute path="/movies/:id" component={MovieForm} /> */}
           <ProtectedRoute path="/about" component={About} />
+          <Route path="/forget-password" component={ForgetPassword} />
+          <Route path="/change-password/:slug" component={ResetPassword} />
           <Home exact path="/" />
         </Switch>
       </BrowserRouter>
